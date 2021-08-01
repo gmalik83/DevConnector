d44961a606840d5cf0c37da8699ac1d9d303e2ca
gmalik83 <gaurav98malik@gmail.com>
2021-08-01 21:32:32 +0530

    DB and Routes added

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API is Running'));
+
+// Defined Routes
+// app.use('/api/users');
 
 const PORT = process.env.PORT || 5000;
 
